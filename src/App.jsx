e1c1c3f2c02b276b3a1b83605744230ec2817392
Shahin Hossain <shahin.hossain.dev@gmail.com>
2025-08-06 18:28:41 +0600

    add: nav, hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
+import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import "./App.css";
-import gsap from "gsap";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
   return (
     <div>
-      <div className="flex-center">
-        <p>Name center</p>
-      </div>
+      <Navbar />
+      <Hero />
     </div>
   );
 }
