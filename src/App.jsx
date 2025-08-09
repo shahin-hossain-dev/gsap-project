65ec1bb74ff3cc860de09f6784e025a55a9fa77c
Shahin Hossain <shahin.hossain.dev@gmail.com>
2025-08-09 17:31:30 +0600

    add: menu section animation

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from "./components/Navbar";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
+import Menu from "./components/Menu";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -17,6 +18,7 @@ function App() {
       <Cocktails />
       <About />
       <Art />
+      <Menu />
     </div>
   );
 }
